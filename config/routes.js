43626315b9f3df9bd50274bc0364a425657ba92c
G0x209C <g@collette-online.nl>
2022-01-17 09:36:37 +0100

    :package: Initial commit

---
+++ config/routes.js
@@ -0,0 +1,34 @@
+/**
+ * Route Mappings
+ * (sails.config.routes)
+ *
+ * Your routes tell Sails what to do each time it receives a request.
+ *
+ * For more information on configuring custom routes, check out:
+ * https://sailsjs.com/anatomy/config/routes-js
+ */
+
+module.exports.routes = {
+
+
+    /**
+     *  /==============
+     * |    API routes
+     *  \==============
+     */
+
+    'POST /api/newplayer': {action:'newplayer'}
+
+    /**
+     *  /==============
+     * |    Chat routes
+     *  \==============
+     */
+
+    /**
+     *  /==============
+     * |    Game routes
+     *  \==============
+     */
+
+};
