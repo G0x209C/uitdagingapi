ae509952af8839d4c944a8811ef13db5b24f9acf
G0x209C <g@collette-online.nl>
2022-01-24 15:56:35 +0100

    :smile: Added method for retrieving room-based messages to Message.js

--- api/models/Message.js
+++ api/models/Message.js
@@ -1,8 +1,22 @@
-module.exports={
+module.exports = {
 
-    attributes:{
-        utterer: {model:'player'},
-        room: {model:'room'},
-        msg: {type:'string', required:true}
+  attributes: {
+    utterer: {model: 'player'},
+    room: {model: 'room'},
+    msg: {type: 'string', required: true}
+  },
+
+  getMessages: async (room) => {
+    let messages;
+    if (room) {
+      messages = await Room.findOne({roomId: room}).populateAll().then((room) => {
+        return room.messages;
+      }).catch(err => {
+        throw err;
+      });
+    } else {
+      throw {badRequest: 'No room provided'};
     }
-}
+    return messages;
+  }
+};
